78ed0abdcdec999a54c2f3aa89af73bd2de6d714
MishC <michaela.chovancova@bergen.kommune.no>
2026-04-15 14:58:10 +0200

    layout for products

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function AuthLayout({ children }: Props) {
+  return (
+    <div className={`flex min-h-svh`}>
+      <div className="fixed inset-y-0 left-0 w-64 max-lg:hidden"> sidebar</div>
+        <main className="flex w-full pb-2 lg:pl-64 lg:pr-4 lg:pt-2">
+            <div className="bg-white grow p-6 lg:rounded-lg lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-zing-950/5">{children}</div></main>
+    </div>
+  );
+}
