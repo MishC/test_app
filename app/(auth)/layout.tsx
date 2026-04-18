1fedf89c34c787768873c2e2465072d04eea8b74
MishC <michaela.chovancova@bergen.kommune.no>
2026-04-18 16:15:43 +0200

    css tailwind fixed

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,14 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Geist, Inter } from "next/font/google";
 import { Sidebar } from "./sidebar";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const sans = Geist({
   subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const alt = Inter({
+  subsets: ["latin"],
+  variable: "--font-alt",
 });
 
 type Props = {
@@ -14,11 +17,20 @@ type Props = {
 
 export default function AuthLayout({ children }: Props) {
   return (
-    <div className={`flex min-h-svh bg-zinc-50 font-sans dark:bg-black ${geistSans.variable} ${inter.variable}`}>
-    
-      <div className="fixed inset-y-0 left-0 w-64 max-lg:hidden"> <Sidebar /> </div>
-        <main className="flex w-full pb-2 lg:pl-64 lg:pr-4 lg:pt-2">
-            <div className="bg-white grow p-6 lg:rounded-lg lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-zing-950/5">{children}</div></main>
+    <div
+      className={`flex min-h-svh bg-zinc-50 font-sans dark:bg-black ${sans.variable} ${alt.variable}`}
+    >
+      {/* Sidebar */}
+      <div className="fixed inset-y-0 left-0 w-64 max-lg:hidden">
+        <Sidebar />
+      </div>
+
+      {/* Content */}
+      <main className="flex w-full pb-2 lg:pl-64 lg:pr-4 lg:pt-2">
+        <div className="grow bg-white p-6 lg:rounded-lg lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-zinc-950/5">
+          {children}
+        </div>
+      </main>
     </div>
   );
 }
