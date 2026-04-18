ad6f4836eee4d9fae816fe4ca02332a45b7414ef
MishC <michaela.chovancova@bergen.kommune.no>
2026-04-18 15:53:56 +0200

    Sidebar func

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Sidebar } from "./sidebar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -14,7 +15,7 @@ type Props = {
 export default function AuthLayout({ children }: Props) {
   return (
     <div className={`flex min-h-svh`}>
-      <div className="fixed inset-y-0 left-0 w-64 max-lg:hidden"> sidebar</div>
+      <div className="fixed inset-y-0 left-0 w-64 max-lg:hidden"> <Sidebar /> </div>
         <main className="flex w-full pb-2 lg:pl-64 lg:pr-4 lg:pt-2">
             <div className="bg-white grow p-6 lg:rounded-lg lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-zing-950/5">{children}</div></main>
     </div>
---
+++ app/(auth)/sidebar.tsx
@@ -0,0 +1,13 @@
+export function Sidebar() {
+    return (
+        <div>
+            <h2>Sidebar</h2>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
