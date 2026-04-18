4d38440533444b2bda160e7f5008444feea91be2
MishC <michaela.chovancova@bergen.kommune.no>
2026-04-18 16:11:22 +0200

    tailwind & Link navigation for  sidebar

--- app/(auth)/sidebar.tsx
+++ app/(auth)/sidebar.tsx
@@ -1,16 +1,12 @@
-import { Package2 } from "lucide-react";
-
+import {Package2 } from "lucide-react";
+import Link from "next/link";
 export function Sidebar() {
     return (
-        <div>
-            <div><Package2 /></div>
-            <h2>Sidebar</h2>
-            <ul>
-                <li>Home</li>
-                <li>Products</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+        <div className="flex flex-col justify-between h-full gap-4 py-2" >
+            <div className="flex h-14 mb-4 items-center border-b px-4 gap-2">
+                <Link href="/" className="flex items-center gap-4 font-semibold"><Package2  className="size-6"/></Link>
+                <span>Next Store</span></div>
+        
         </div>
     )
 }
