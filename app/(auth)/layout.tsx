44155273360d225a73333e41ed74ef80fa15a652
MishC <michaela.chovancova@bergen.kommune.no>
2026-04-18 16:21:50 +0200

    Geist font used only

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,14 @@
-import { Geist, Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import { Sidebar } from "./sidebar";
 
-const sans = Geist({
+const geistSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-const alt = Inter({
+const geistMono = Geist_Mono({
   subsets: ["latin"],
-  variable: "--font-alt",
+  variable: "--font-geist-mono",
 });
 
 type Props = {
@@ -18,14 +18,12 @@ type Props = {
 export default function AuthLayout({ children }: Props) {
   return (
     <div
-      className={`flex min-h-svh bg-zinc-50 font-sans dark:bg-black ${sans.variable} ${alt.variable}`}
+      className={`flex min-h-svh bg-zinc-50 dark:bg-black ${geistSans.variable} ${geistMono.variable}`}
     >
-      {/* Sidebar */}
-      <div className="fixed inset-y-0 left-0 w-64 max-lg:hidden">
+      <div className="fixed inset-y-0 left-0 hidden w-64 lg:block">
         <Sidebar />
       </div>
 
-      {/* Content */}
       <main className="flex w-full pb-2 lg:pl-64 lg:pr-4 lg:pt-2">
         <div className="grow bg-white p-6 lg:rounded-lg lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-zinc-950/5">
           {children}
