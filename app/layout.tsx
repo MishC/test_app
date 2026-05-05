e09b2cbcc0b1c9f46b1aea1b2fd94a80ef370177
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-05 13:23:17 +0200

    convex_clerk integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ClerkProvider } from '@clerk/nextjs'
+import {ConvexClientProvider} from "./convex-client-provider";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -33,9 +33,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>
+        <ConvexClientProvider>
           {children}
-        </ClerkProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
