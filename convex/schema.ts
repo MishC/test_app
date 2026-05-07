141eb192eae249d6f6c70da3f81281b5c85708ea
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-08 01:43:15 +0200

    db schema + mutatuon:db.insert

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,6 +5,11 @@ export default defineSchema({
 
   users: defineTable({
     clerkId: v.string(),
+    username: v.optional(v.string()),
+    email: v.optional(v.string()),
+    name: v.optional(v.string()),
+    about:v.optional(v.string()),
+    logo:v.optional(v.string()),
   
-  }),
+  })
 });
--- convex/users.ts
+++ convex/users.ts
@@ -1,12 +1,38 @@
-import {query} from "./_generated/server";
-import {v} from "convex/values";
+import { internalMutation, internalQuery, query } from "./_generated/server";
+import { v } from "convex/values";
+import { generateUsername } from "friendly-username-generator";
 
 export const getUsers = query({
   args: {
     userId: v.id('users'),
   },
-  handler: async (ctx, {userId}) => {
+  handler: async (ctx, { userId }) => {
     const users = await ctx.db.get(userId);
     return users;
   }
+})
+
+export const createUser = internalMutation({
+  args: {
+    clerkId: v.string(),
+    name: v.optional(v.string()),
+    email: v.optional(v.string()),
+    username: v.optional(v.string()),
+    about: v.optional(v.string()),
+    logo: v.optional(v.string()),
+
+
+
+  },
+  handler: async (ctx, { clerkId, username, email, name, about, logo }) => {
+    const userId = await ctx.db.insert("users", {
+      clerkId,
+      username: username || generateUsername(),
+      email,
+      name,
+      about,
+      logo
+    });
+    return userId;
+  },
 })
