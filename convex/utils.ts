23f43287356304815647e388172b9aa231e0bd0d
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-08 11:09:19 +0200

    new queries functions

--- convex/utils.ts
+++ convex/utils.ts
@@ -1,7 +1,8 @@
 import { Auth } from "convex/server";
 import {customAction, customCtx,  customMutation, customQuery} from "convex-helpers/server/customFunctions";
-import { action, mutation, query } from "./_generated/server";
-import { ConvexError } from "convex/values";
+import { action, DatabaseReader, mutation, query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { get } from "http";
 
 async function getUserId(ctx: {auth:Auth}) {
  const authInfo = await ctx.auth.getUserIdentity();
@@ -36,3 +37,65 @@ export const actionWithUser=customAction(
         return{ userId};
     })
 );
+
+queryWithUser({
+    args:{
+        clerkId:v.string()}, 
+handler: async (ctx,args)=>{
+    const user= await getUserByClerkId(ctx.db, args.clerkId);
+    return user;
+},
+});
+
+
+export const getUserByClerkId= async(db:DatabaseReader, clerkId:string)=>{
+    const user= await db.query('users')
+    .withIndex('by_clerkId', (q)=>q.eq('clerkId', clerkId))
+    .first();
+    return user;
+}
+
+
+export const getUserByEmail= async(db:DatabaseReader, email:string)=>{
+    const user= await db.query('users')
+    .withIndex('by_email', (q)=>q.eq('email', email))
+    .first();
+    return user;
+}
+
+export const getProductsByClerkId= async(db:DatabaseReader, clerkId:string)=>{
+    const products= await db.query('products')
+    .withIndex('by_clerkId', (q)=>q.eq('clerkId', clerkId))
+    .collect();
+    return products;
+}   
+
+export const getSalesByStoreClerkId= async(db:DatabaseReader, clerkId:string)=>{
+    const sales= await db.query('sales')
+    .withIndex('by_storeClerkId', (q)=>q.eq('storeClerkId', clerkId))
+    .collect();
+    return sales;
+}
+
+export const getSalesByCustomerClerkId= async(db:DatabaseReader, clerkId:string)=>{
+    const sales= await db.query('sales')
+    .withIndex('by_customerClerkId', (q)=>q.eq('customerClerkId', clerkId))
+    .collect();
+    return sales;
+}
+
+export const getSalesByProductId= async(db:DatabaseReader, productId:string)=>{
+    const sales= await db.query('sales')
+    .withIndex('by_productId', (q)=>q.eq('productId', productId))
+    .collect();
+    return sales;
+}
+
+
+
+export const getKeyByClerkId= async(db:DatabaseReader, clerkId:string)=>{
+    const key= await db.query('keys')
+    .withIndex('by_clerkId', (q)=>q.eq('clerkId', clerkId))
+    .first();
+    return key;
+}
