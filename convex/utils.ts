ac5375ce71394b627ef18c1c87f9a1c3fb84b414
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-08 09:19:32 +0200

    helper function for authorisation: logged/not logged ?

---
+++ convex/utils.ts
@@ -0,0 +1,38 @@
+import { Auth } from "convex/server";
+import {customAction, customCtx, customCtx, customMutation, customQuery} from "convex-helpers/server/customFunctions";
+import { action, mutation, query } from "./_generated/server";
+import { ConvexError } from "convex/values";
+
+async function getUserId(ctx: {auth:Auth}) {
+ const authInfo = await ctx.auth.getUserIdentity();
+ return authInfo?.subject;
+    }
+export const queryWithUser=customQuery(
+    query,
+    customCtx(async (ctx) => {
+        return{
+   userId: await getUserId(ctx)
+        };
+    })
+);
+
+export const mutationWithUser=customMutation(
+    mutation,
+    customCtx(async (ctx) => {
+        const userId= await getUserId(ctx);
+        if (!userId) {
+            throw new ConvexError(`Unauthorized. You must be logged in to perform this action.`);}
+        return{ userId};
+    })
+);
+
+
+export const actionWithUser=customAction(
+    action,
+    customCtx(async (ctx) => {
+        const userId= await getUserId(ctx);
+        if (!userId) {
+            throw new ConvexError(`Unauthorized. You must be logged in to perform this action.`);}
+        return{ userId};
+    })
+);
