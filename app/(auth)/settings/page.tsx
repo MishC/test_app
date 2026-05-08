21417af1257aaba8bb90c05c6e5606d855b8ccc7
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-08 12:36:44 +0200

    new folders/pages

---
+++ app/(auth)/settings/page.tsx
@@ -0,0 +1,9 @@
+import { ContentLayout } from "../content-layout"
+export default function SettingsPage() {
+    return (
+        <div>
+            <ContentLayout title="Settings" description="Manage your account settings.">
+    $$            </ContentLayout>
+        </div>
+    )
+}
