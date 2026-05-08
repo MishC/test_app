5070ed1194458140e5fb5400a4c27961e7124a8f
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-08 09:32:02 +0200

    new tables : products, sales, keys

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,5 +1,6 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
+import { PHASE_PRODUCTION_BUILD } from "next/dist/shared/lib/constants";
 
 export default defineSchema({
 
@@ -8,8 +9,32 @@ export default defineSchema({
     username: v.optional(v.string()),
     email: v.optional(v.string()),
     name: v.optional(v.string()),
-    about:v.optional(v.string()),
-    logo:v.optional(v.string()),
-  
-  })
+    about: v.optional(v.string()),
+    logo: v.optional(v.string()),
+
+  }),
+  products: defineTable({
+    clerkId: v.string(),
+    name: v.string(),
+    description: v.string(),
+    content: v.string(),
+    price: v.number(),
+    currency: v.optional(v.string()),
+    coverImage: v.optional(v.string()),
+    published: v.boolean(),
+
+  }),
+
+  sales: defineTable({
+    storeClerkId: v.string(),
+
+    customerClerkId: v.string(),
+    productId: v.string(),
+    price: v.number(),
+    currency: v.optional(v.string()),
+  }),
+  keys: defineTable({
+    clerkId: v.string(),
+    stripeKey: v.string(),
+  }),
 });
