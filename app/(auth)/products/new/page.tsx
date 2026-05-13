b043d321b0d9cdd5d1a96ee4494ae4607a98d6d3
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-13 12:27:44 +0200

    newProduct

---
+++ app/(auth)/products/new/page.tsx
@@ -0,0 +1,9 @@
+import { ContentLayout } from "../../content-layout";
+
+export default function NewProductPage() {
+    return (
+       <ContentLayout title="New Product" description="Add a new product to your store.">
+  <div></div>
+       </ContentLayout>
+    )
+}
