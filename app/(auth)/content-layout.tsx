3e68e832670e40ae7a31091bcac79b2110050282
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-15 10:33:22 +0200

    css content layout

--- app/(auth)/content-layout.tsx
+++ app/(auth)/content-layout.tsx
@@ -10,8 +10,8 @@ export function ContentLayout({ icon=null,title, description, action, children }
     <>
       <header className="flex justify-between items-center mb-8">
         <div>
-         <span className="flex"> <span className="text-2xl font-bold tracking-tight mb-2 mr-2">{icon}</span>
-          <h2 className="text-2xl font-bold tracking-tight mb-2">{title}</h2></span>
+         <span className="flex"> {icon?<span className="text-2xl font-bold tracking-tight mb-2 mr-2">{icon}</span>
+         :""} <h2 className="text-2xl font-bold tracking-tight mb-2">{title}</h2></span>
           <p className="text-muted-foreground">{description}</p>
         </div>
         {action}
