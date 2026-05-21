ba236a0ffd3a2b769c49d801730375bc15835c2b
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-21 10:11:28 +0200

    fixed: focus-visible : outline-2

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
             href="/products"
             className="max-w-1 w-1 rounded-lg
              bg-white px-4 py-2 text-sm font-medium text-zinc-950 shadow-sm ring-1 ring-zinc-950/10 hover:bg-zinc-50 
-             focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-950/10"
+             focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-950/10"
           >
             View Products
           </Link>
