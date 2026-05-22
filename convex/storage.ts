08b9db53b5b95350b3b9d7212b7b9fd0bafe7e5e
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-22 09:08:20 +0200

    renewed to v.id("name_of_table") from depreacated v.string()

--- convex/storage.ts
+++ convex/storage.ts
@@ -1,13 +1,13 @@
 import {mutation} from "./_generated/server";
 import { v } from "convex/values";
-//CONVEX API FOR FILE STORAGE
+//Convex API for file storage
 export const generateUploadUrl = mutation(async(ctx)=>{
     return await ctx.storage.generateUploadUrl();
 
 })
 
 export const getFileUrl = mutation({
-    args:{storageId:v.string()},
+    args:{storageId: v.id("_storage")},
     handler: (ctx, {storageId})=>{
         return  ctx.storage.getUrl(storageId);
     }
