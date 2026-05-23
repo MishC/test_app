6260f5a2378524befce3a2107bb2552a29fed90a
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-23 16:22:36 +0200

    Buy Button added and SignInButton added, based on auth

---
+++ app/(store)/[username]/[productId]/buy-button.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Doc } from "@/convex/_generated/dataModel";
+
+type Props={
+    product:Doc<'products'>
+}
+export function BuyButton({product}:Props){
+    return(
+       <Button>Buy now</Button>
+    )
+}
--- app/(store)/[username]/[productId]/page.tsx
+++ app/(store)/[username]/[productId]/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { fetchQuery } from "convex/nextjs";
+import Link from "next/link";
 import { notFound } from "next/navigation";
+import { BuyButton } from "./buy-button";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 type Props = {
   params: Promise<{
@@ -20,7 +25,7 @@ export default async function StoreProductPage({ params }: Props) {
   if (!product) {
     notFound();
   }
-
+  const { userId } = await auth();
 return (
     <div className="flex justify-center w-full sm:p-12 items-center ">
         <div className="w-full max-w-3xl border border-zinc-300 shadow-md overflow-hidden sm:rounded-lg">
@@ -29,7 +34,13 @@ return (
 :<div className="mb-2 h-48 bg-zinc-200"></div>}
       
     <div className="flex justify-between p-4 items-center">
-        <h1 className="text-3xl font-semibold"></h1>
+        <h1 className="text-3xl font-semibold">{product.name}</h1>
+       <div className="flex items-center">
+        <img src={product.user?.logo} className="rounded-full size-6 border-black border shadow-sm"/>
+       <Button asChild variant="link"><Link href={`/${product.user?.username}`}>{product.user?.username}</Link></Button>
+       </div>
+
+{userId ? <BuyButton product={product} /> : <SignInButton><Button>Sign in to buy</Button></SignInButton>}
     </div>
       </div>
     </div>
