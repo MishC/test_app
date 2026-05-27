080c0d3a5ed7c284a55b7a74fd41fa0c5a69fef5
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-27 03:17:30 +0200

    library page

---
+++ app/(auth)/library/page.tsx
@@ -0,0 +1,22 @@
+import { ContentLayout } from "../content-layout";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default async function Library() {
+    return(
+        <div>
+            <ContentLayout
+      title="Library"
+      description="View all your purchased products."
+      action={
+        <Link href="/products/new">
+          <Button className="px-5 py-5">New Product</Button>
+        </Link>
+      }
+    >
+    <ul></ul>
+         </ContentLayout>
+  
+        </div>
+    )
+}
