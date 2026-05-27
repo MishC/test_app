ae7fe0ef2c6975fd9c609773f07d8197e7ead0d5
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-27 10:02:34 +0200

    update type of productId

--- app/(auth)/library/[productId]/page.tsx
+++ app/(auth)/library/[productId]/page.tsx
@@ -1,25 +1,28 @@
 import { getAuthToken } from "@/lib/getAuthToken";
 import { ContentLayout } from "../../content-layout";
 import { fetchQuery } from "convex/nextjs";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 
 type Props={
-    params:{productId:Id<"products">}
+    params:Promise<{productId:string}>
 }
 
 export default async function LibraryProduct({params}:Props){
+    const { productId } = await params;
     const token= await getAuthToken();
     if (!token){
         redirect("/sign-in");
     }
-    const product= await fetchQuery(api.library.getLibraryProduct,{productId:params.productId},{token})
-
+    const product= await fetchQuery(api.library.getLibraryProduct,{productId:productId as Id<"products">},{token})
+     if (!product){
+        notFound()
+     }
     return(
         <ContentLayout title={product.name} description={product.description}>
             <div className="prose">
-                <div dangerouslySetInnerHTML={{__html:product.content}}></div>
+                <div dangerouslySetInnerHTML={{__html:product.content}}/>
             </div>
             </ContentLayout>
     )
