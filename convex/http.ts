9cf3e5352e68526ede7c8b910684125c07f2517f
MishC <michaela.chovancova@bergen.kommune.no>
2026-05-27 02:44:43 +0200

    Stripe webhook connected with Convex  internal mutation

--- convex/http.ts
+++ convex/http.ts
@@ -1,8 +1,23 @@
 import { HttpRouter } from "convex/server";
 
 import { onCreateUser } from "./clerk.js";
+import {  httpAction } from "./_generated/server.js";
+import { internal } from "./_generated/api.js";
+
 const http= new HttpRouter();
 http.route({ path:"/clerk", method:"POST",
     handler:onCreateUser,
+});
+http.route({ path:"/stripe", method:"POST",
+    handler:httpAction( async (ctx,request)=>{
+const signature=request.headers.get('stripe-signature');
+const result=await ctx.runAction(internal.stripe.fulfill,{payload: await request.text(),signature});
+if (result.success){
+    return new Response(null, {status:200})
+} else {
+        return new Response("Webhook Error", {status:400})
+
+}
+    }),
 })
 export default http;
