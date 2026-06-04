b0124e8ed72ba1aaee370fee1f71a80f8da2cc5f
MishC <michaela.chovancova@bergen.kommune.no>
2026-06-04 18:42:40 +0200

    added html attribute suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn("font-sans", geist.variable)} suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
